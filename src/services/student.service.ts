e60362720ab385f078e82a4cc4bbc04e375c0570
Ricardo <ricardo.dev1991@gmail.com>
2024-11-02 10:55:48 -0300

    fix

--- src/services/student.service.ts
+++ src/services/student.service.ts
@@ -7,7 +7,7 @@ import { v4 as uuid } from 'uuid';
 
 const create = (name: string, email: string, course: string) => {
     const newStudent = new Student(uuid(), name, email, course);
-    const studentEmailExists = StudentRepository.findByEmail(email);
+    const studentEmailExists = StudentRepository.findByEmail(newStudent.email);
 
     if (studentEmailExists) {
         throw new Error('Este e-mail já está cadastrado.');
